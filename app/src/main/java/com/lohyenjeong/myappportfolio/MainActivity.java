6b791893f021c14ac750ce9b05ab0a3b7a500498
lohyenjeong <lohyenjeong@gmail.com>
2016-06-20 23:18:17 +0100

    edited_colour_scheme_and_refactored_code

--- app/src/main/java/com/lohyenjeong/myappportfolio/MainActivity.java
+++ app/src/main/java/com/lohyenjeong/myappportfolio/MainActivity.java
@@ -12,67 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button bPopularMovies = (Button) findViewById(R.id.popular_movies);
-        bPopularMovies.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.popular_movies).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-        Button bHawk = (Button) findViewById(R.id.stock_hawk);
-        bHawk.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.stock_hawk).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-        Button bBigger = (Button) findViewById(R.id.build_it_bigger);
-        bBigger.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.build_it_bigger).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-        Button bMaterial = (Button) findViewById(R.id.material);
-        bMaterial.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.material).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-        Button bUbi = (Button) findViewById(R.id.go_ubiquitous);
-        bUbi.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.go_ubiquitous).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-        Button bCapstone = (Button) findViewById(R.id.capstone);
-        bCapstone.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String msg = "This button will launch my " + getString(R.string.capstone).toLowerCase() + " project";
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
     }
 
-
+    /** Called when the user touches the button */
+    public void createToast(View view) {
+        // Do something in response to button click
+        Button b = (Button)view;
+        String buttonText = b.getText().toString().toLowerCase();
+        Toast.makeText(MainActivity.this, "This is my project " + buttonText, Toast.LENGTH_SHORT).show();
+    }
 }
+
+
+
